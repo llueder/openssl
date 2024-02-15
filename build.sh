4a64be0bbf2b798d9025a2860214247ecdd3b18b
Lasse Lüder <lasse.lueder@lueder-mail.de>
2024-02-15 18:43:24 +0100

    update build scripts

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
-MTUNE=raptorlake
-DIR=./libcrypto_${MTUNE}
-CFLAGS="-mtune=${MTUNE} -ffunction-sections" CPPFLAGS="-mtune=${MTUNE} -ffunction-sections" ./config -static --static
-make -j4 || echo "ignore error"
-mkdir -p ${DIR}
-rm -rf ${DIR}/*
-ar --output=${DIR} x libcrypto.a
+#!/bin/sh
 
-MTUNE=tremont
+MTUNE=$1
 DIR=./libcrypto_${MTUNE}
 CFLAGS="-mtune=${MTUNE} -ffunction-sections" CPPFLAGS="-mtune=${MTUNE} -ffunction-sections" ./config -static --static
 make -j4 || echo "ignore error"
---
+++ buildRaptorlakeAndTremont.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+./build.sh raptorlake
+./build.sh tremont
