5a2bb4dc4d14ce0e43fcbf036cc1de1641d8e4cd
Lasse Lüder <lasse.lueder@tuhh.de>
2024-02-24 18:57:57 +0000

    adapt naming of variants

--- buildRaptorlakeAndTremont.sh
+++ buildRaptorlakeAndTremont.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-
 ./build.sh raptorlake
+for f in $(ls libcrypto_raptorlake/*.o); do
+    mv $f ${f%.o}_A.o
+done
 ./build.sh tremont
+for f in $(ls libcrypto_tremont/*.o); do
+    mv $f ${f%.o}_B.o
+done
